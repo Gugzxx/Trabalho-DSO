8896f3d63cd8fdd939a7c59e531c4c15c5ad4b9b
Gugzxx <gustavo07genovez@gmail.com>
2025-10-06 21:25:02 -0300

    Update ControladorReserva.py

--- ControladorReserva.py
+++ ControladorReserva.py
@@ -3,31 +3,31 @@ class ControladorReserva:
     self.__tela_reserva : TelaReserva(self)
     reservas : []
 
-def inicia(self):
-  self.abre_tela_inicial()
-
-def solicitar_reserva(self):
-  ...
-
-def confirmar_reserva(self):
-  ...
-
-def cancelar_reserva(self):
-  ...
-
-def consultar_reserva(self):
-  ...
-
-def lista_reserva(self):
-  ...
-
-def finalizar(self):
-  exit(0)
-
-def abre_tela_inicial(self):
-  switcher = (0: finalizar, 1: self.solicitar_reserva, 2: self.confirmar_reserva, 3: self.cancelar_reserva, 4: self.consultar_status, 5: self.lista_reservas)
-
-  while True:
-    opcao = self.__tela_reserva.mostra_tela_opcoes()
-    funcao_escolhida = switcher[opcao]
-    funcao_escolhida()
+  def inicia(self):
+    self.abre_tela_inicial()
+  
+  def solicitar_reserva(self):
+    ...
+  
+  def confirmar_reserva(self):
+    ...
+  
+  def cancelar_reserva(self):
+    ...
+  
+  def consultar_reserva(self):
+    ...
+  
+  def lista_reserva(self):
+    ...
+  
+  def finalizar(self):
+    exit(0)
+  
+  def abre_tela_inicial(self):
+    switcher = (0: finalizar, 1: self.solicitar_reserva, 2: self.confirmar_reserva, 3: self.cancelar_reserva, 4: self.consultar_status, 5: self.lista_reservas)
+  
+    while True:
+      opcao = self.__tela_reserva.mostra_tela_opcoes()
+      funcao_escolhida = switcher[opcao]
+      funcao_escolhida()
