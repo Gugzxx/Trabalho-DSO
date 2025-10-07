b2587a1b59338bb451d007afba70667fe3da35d0
Gugzxx <gustavo07genovez@gmail.com>
2025-10-06 21:22:14 -0300

    Update AbstractTela.py

--- AbstractTela.py
+++ AbstractTela.py
@@ -14,14 +14,3 @@ class AbstractTela:
           print("Valor incorreto: Digite um numero inteiro valido")
           if inteiros_validos:
             print("Valores validos: ", inteiros_validos)
-
-  def mostra_tela_opcoes(self):
-    print("-------- Realizar Reserva --------")
-    print("1 - Solicitar Reserva")
-    print("2 - Confirmar Reserva")
-    print("3 - Cancelar Reserva")
-    print("4 - Consultar Status da Reserva")
-    print("5 - Lista das Reservas")
-    print("0 - Finalizar")
-    opcao = self.le_num_inteiro("Escolha a opcao: ", [1, 2, 3, 4, 5, 0])
-    return opcao
