c05e1f696e49c70166afdde128d4bae09b0e5fc8
Gugzxx <gustavo07genovez@gmail.com>
2025-10-06 21:14:02 -0300

    Update AbstractTela.py

--- AbstractTela.py
+++ AbstractTela.py
@@ -2,18 +2,18 @@ class AbstractTela:
   def __init__(self, controlador):
     self.__controlador = controlador
 
-def le_num_inteiro(self, mensagem: str = '', inteiros_validos: [] = None):
-  while True:
-    valor_lido = input(mensagem)
-    try:
-        inteiro = int(valor_lido)
-        if inteiros_validos and inteiro not in inteiros_validos:
-            raise ValueError
-        return inteiro
-    except ValueError:
-        print("Valor incorreto: Digite um numero inteiro valido")
-        if inteiros_validos:
-          print("Valores validos: ", inteiros_validos)
+  def le_num_inteiro(self, mensagem: str = '', inteiros_validos: [] = None):
+    while True:
+      valor_lido = input(mensagem)
+      try:
+          inteiro = int(valor_lido)
+          if inteiros_validos and inteiro not in inteiros_validos:
+              raise ValueError
+          return inteiro
+      except ValueError:
+          print("Valor incorreto: Digite um numero inteiro valido")
+          if inteiros_validos:
+            print("Valores validos: ", inteiros_validos)
 
   def mostra_tela_opcoes(self):
     print("-------- Realizar Reserva --------")
