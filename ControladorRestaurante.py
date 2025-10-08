e9fab8a6ab40143ca360b189474f5e74eb6d5118
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-08 08:56:26 -0300

    Update ControladorRestaurante.py

--- ControladorRestaurante.py
+++ ControladorRestaurante.py
@@ -1,1 +1,25 @@
-class controladorrest
+from Restaurante import Restaurante
+from Mesa import Mesa
+
+class ControladorRestaurante:
+  def __init__(self, restaurante):
+      self.__restaurante = restaurante
+    
+  def incluir_mesa(self, numero, qtd_lugares):
+    nova_mesa = Mesa(numero, qtd_lugares)
+    self.__restaurante.incluir_mesa(nova_mesa)
+    return nova_mesa
+    
+  def excluir_mesa(self, numero):
+    for mesa in self.__restaurante:
+      if mesa.numero == numero:
+        self.__restaurante.excluir_mesa(mesa)
+        return True
+    return False
+    
+  def listar_mesa(self):
+    return.__restaurante.mesas
+    
+  
+
+  
