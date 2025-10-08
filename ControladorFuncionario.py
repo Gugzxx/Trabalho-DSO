2d77958a7e126a15e0cbf5e620b7f611014f447a
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-08 10:35:31 -0300

    Create ControladorFuncionario.py

---
+++ ControladorFuncionario.py
@@ -0,0 +1,18 @@
+from Funcionario import Funcionario
+from ControladorReserva import ControladorReserva
+
+class ControladorFuncionario:
+    def __init__(self, controlador_reserva):
+        self.__controlador_reserva = controlador_reserva
+        self.__funcionarios = []
+
+    def cadastrar_funcionario(self, nome, cargo):
+        funcionario = Funcionario(nome, cargo)
+        self.__funcionarios.append(funcionario)
+        return funcionario
+
+    def anotar_reserva(self, funcionario, dados_reserva):
+        return self.__controlador_reserva.solicitar_reserva(funcionario, dados_reserva)
+
+    def alterar_reserva(self, funcionario, id_reserva, novos_dados):
+        return self.__controlador_reserva.alterar_reserva(funcionario, id_reserva, novos_dados)
