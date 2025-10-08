3bd261e7cad240434e2bb984d4155cac443ab6cb
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-08 10:02:57 -0300

    Update ControladorUsuario.py

--- ControladorUsuario.py
+++ ControladorUsuario.py
@@ -6,18 +6,20 @@ class ControladorUsuario:
     self.__controlador_reserva = controlador_reserva
     self.__usuarios = []
 
-  def cadastrar_usuario:
+  def cadastrar_usuario(self, nome, telefone, email):
     novo_usuario = Usuario(nome, telefone, email)
     self.__usuarios.append(novo_usuario)
     return novo_usuario
 
-  def listar_usuarios:
+  def listar_usuarios(self):
     return self.__usuarios
 
   def anotar_reserva(self, usuario, dados_reserva):
     return self.__controlador_reserva.solicitar_reserva(usuario, dados_reserva)
 
-  def
+  def alterar_reserva(self, usuario, id_reserva, novoa_dados):
+    return self.__controlador_reserva.alterar_reserva(usuario, id_reserva, novos_dados)
+    
   
 
 
