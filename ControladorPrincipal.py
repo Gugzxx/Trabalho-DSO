72dd56d22ece53376e45acfa5634046416d07f5f
Gugzxx <gustavo07genovez@gmail.com>
2025-10-10 16:15:27 -0300

    Update ControladorPrincipal.py

--- ControladorPrincipal.py
+++ ControladorPrincipal.py
@@ -1,36 +1,34 @@
 def main():
     from ControladorReserva import ControladorReserva
-    from ControladorUsuario import ControladorUsuario
     from ControladorCliente import ControladorCliente
     from ControladorFuncionario import ControladorFuncionario
 
     controlador_reserva = ControladorReserva()
-    controlador_usuario = ControladorUsuario(controlador_reserva)
     controlador_cliente = ControladorCliente(controlador_reserva)
     controlador_funcionario = ControladorFuncionario(controlador_reserva)
     
     while True:
         print("\n=== Menu Principal ===")
-        print("1 - Menu Usuário")
-        print("2 - Menu Cliente")
-        print("3 - Menu Funcionário")
-        print("4 - Menu Reserva")
+        print("1 - Menu Cliente")
+        print("2 - Menu Funcionário")
+        print("3 - Menu Reserva")
         print("0 - Sair")
         opcao = input("Escolha uma opção: ")
 
         if opcao == "1":
-            controlador_usuario._ControladorUsuario__tela_usuario.mostrar_opcoes()
+            controlador_cliente._ControladorCliente__tela_cliente.mostrar_tela_opcoes()
         elif opcao == "2":
-            controlador_cliente._ControladorCliente__tela_cliente.mostrar_opcoes()
+            controlador_funcionario._ControladorFuncionario__tela_funcionario.mostrar_tela_opcoes()
         elif opcao == "3":
-            controlador_funcionario._ControladorFuncionario__tela_funcionario.mostrar_opcoes()
-        elif opcao == "4":
             controlador_reserva.inicia()
         elif opcao == "0":
             print("Saindo do sistema.")
             break
         else:
             print("Opção inválida!")
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
