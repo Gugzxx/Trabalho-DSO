b7b50d5625834674c9339a98ccea951a7d0bd92d
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:17:43 -0300

    Update ControladorRestaurante.py

--- ControladorRestaurante.py
+++ ControladorRestaurante.py
@@ -1,21 +1,49 @@
-from Restaurante import Restaurante
-from Mesa import Mesa
+from Objetos.Restaurante import Restaurante
+from Objetos.Cardapio import Cardapio
 
 class ControladorRestaurante:
-  def __init__(self, restaurante):
-    self.__restaurante = restaurante
+    def __init__(self, sistema, tela_restaurante):
+        self.sistema = sistema
+        self.tela_restaurante = tela_restaurante
     
-  def incluir_mesa(self, numero, qtd_lugares):
-    nova_mesa = Mesa(numero, qtd_lugares)
-    self.__restaurante.incluir_mesa(nova_mesa)
-    return nova_mesa
     
-  def excluir_mesa(self, numero):
-    for mesa in self.__restaurante.mesas:
-      if mesa.numero == numero:
-        self.__restaurante.excluir_mesa(mesa)
-        return True
-    return False
+    def gerenciar_cardapio(self):
+        nome_restaurante = input("Nome do restaurante: ")
+        restaurante = next((r for r in self.sistema.restaurantes if r.nome == nome_restaurante), None)
+        if not restaurante:
+            print("Restaurante não encontrado.")
+            return
+        if not restaurante.cardapio:
+            restaurante.cardapio = Cardapio(restaurante.nome)
+        print("1 - Adicionar item")
+        print("2 - Listar itens")
+        escolha = input("Escolha: ")
+        if escolha == "1":
+            item = input("Digite o nome do item: ")
+            restaurante.cardapio.adicionar_item(item)
+            print("Item adicionado.")
+        elif escolha == "2":
+            for item in restaurante.cardapio.listar_itens():
+                print(item)
+
+    def menu_restaurante(self):
+        while True:
+            escolha = self.tela_restaurante.mostrar_menu()
+            if escolha == "1":
+                self.tela_restaurante.mostrar_restaurantes(self.sistema.restaurantes)
+            elif escolha == "2":
+                nome = input("Nome do restaurante: ")
+                endereco = input("Endereço: ")
+                restaurante = Restaurante(nome, endereco)
+                self.sistema.restaurantes.append(restaurante)
+                self.tela_restaurante.mostrar_mensagem("Restaurante adicionado.")
+            elif escolha == "3":
+                self.gerenciar_cardapio()
+            elif escolha == "4":
+                self.listar_pagamentos()
+            elif escolha == "0":
+                break
+            else:
+                self.tela_restaurante.mostrar_mensagem("Opção inválida.")
+
     
-  def listar_mesas(self):
-    return self.__restaurante.mesas
