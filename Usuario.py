b57cf8684794af62d65cd5423749817a68d7e875
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:20:53 -0300

    Update Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -1,45 +1,18 @@
-from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
+class Usuario:
+    def __init__(self, login, senha, tipo):
+        self.login = login
+        self.senha = senha
+        self.tipo = tipo
 
-if TYPE_CHECKING:
-    from Reserva import Reserva
+class Funcionario(Usuario):
+    def __init__(self, login, senha, nome, email, is_admin=False):
+        super().__init__(login, senha, "funcionario")
+        self.nome = nome
+        self.email = email
+        self.is_admin = is_admin
 
-class Usuario(ABC):
-    def __init__(self, nome: str, telefone: str, email: str):
-        self.__nome = None
-        self.__telefone = None
-        self.__email = None
-
-        if isinstance(nome, str):
-            self.__nome = nome
-        if isinstance(telefone, str):
-            self.__telefone = telefone
-        if isinstance(email, str):
-            self.__email = email
-    
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome: str):
-        if isinstance(nome, str) and len(nome) > 0:
-            self.__nome = nome
-
-    @property
-    def telefone(self):
-        return self.__telefone
-
-    @telefone.setter
-    def telefone(self, telefone: str):
-        if isinstance(telefone, str) and len(telefone) > 0:
-            self.__telefone = telefone
-
-    @property
-    def email(self):
-        return self.__email
-
-    @email.setter
-    def email(self, email: str):
-        if isinstance(email, str) and "@" in email:
-            self.__email = email
+class Cliente(Usuario):
+    def __init__(self, login, senha, nome, email):
+        super().__init__(login, senha, "cliente")
+        self.nome = nome
+        self.email = email
