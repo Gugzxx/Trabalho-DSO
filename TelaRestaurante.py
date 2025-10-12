5b2bc872a9814c98ce73ad3904ffe091c23f0263
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:22:24 -0300

    Update TelaRestaurante.py

--- TelaRestaurante.py
+++ TelaRestaurante.py
@@ -1,56 +1,17 @@
-from AbstractTela import AbstractTela
-from Restaurante import Restaurante
+from Telas.AbstractTela import AbstractTela
 
-class TelaRestaurante:
-  def __init__(self, controlador):
-    self.__controlador = controlador
 
-  def mostra_tela_opcoes(self):
-    while True:
-      print("/n=== Menu Restaurante ===")
-      print("1 - Incluir Mesa")
-      print("2 - Excluir Mesa")
-      print("3 - Listar Mesas")
-      print("0 - Sair")
-      opcao = input("Escolha: ")
+class TelaRestaurante(AbstractTela):
+    def mostrar_menu(self):
+        print("\nMenu Restaurante:")
+        print("1 - Listar Restaurantes")
+        print("2 - Adicionar Restaurante")
+        print("0 - Voltar")
+        return input("Escolha: ")
 
-      if opcao == "1":
-        while True:
-          try:
-            numero = int(input("Número da mesa: "))
-            break
-          except ValueError:
-            print("Digite um número inteiro válido.")
-        while True:
-          try:
-            qtd_lugares = int(input("Quantidade de lugares: "))
-            break
-          except ValueError:
-            print("Digite um número inteiro válido.")
-        self.__controlador.incluir_mesa(numero, qtd_lugares)
-        print("Mesa incluída com suceesso!")
-
-      elif opcao == "2":
-        while True:
-          try:
-              numero = int(input("Número da mesa a excluir: "))
-              break
-          except ValueError:
-              print("Digite um número inteiro válido.")
-        if self.__controlador.excluir_mesa(numero):
-          print("Mesa exclúida com sucesso!")
+    def mostrar_restaurantes(self, restaurantes):
+        if not restaurantes:
+            print("Nenhum restaurante cadastrado.")
         else:
-          print("Mesa não encontrada!")
-          
-      elif opcao == "3":
-        mesas = self.__controlador.listar_mesas()
-        print("Mesas cadasttradas: ")
-        for mesa in mesas:
-          print(f"Mesa {mesa.numero} - Lugares: {mesa.qtd_lugares}")
-
-      elif opcao == "0":
-        print("Saindo do Menu.")
-        break
-     
-      else:
-        print("Opção Inválida.")
+            for r in restaurantes:
+                print(f"Nome: {r.nome} | Endereço: {r.endereco}")
