0a617295980b402da4ec14a01b481e7c0165d42d
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:19:23 -0300

    Update Mesa.py

--- Mesa.py
+++ Mesa.py
@@ -1,27 +1,5 @@
 class Mesa:
-    def __init__(self, numero: int, qtd_lugares: int):
-        self.__numero = None
-        self.__qtd_lugares = None
-        self.__status = "Disponível"
-
-        if isinstance(numero, int):
-            self.__numero = numero
-        if isinstance(qtd_lugares, int):
-            self.__qtd_lugares = qtd_lugares
-
-    @property
-    def numero(self):
-        return self.__numero
-
-    @property
-    def qtd_lugares(self):
-        return self.__qtd_lugares
-
-    @property
-    def status(self):
-        return self.__status
-
-    @status.setter
-    def status(self, status: str):
-        if status in ("Disponível", "Reservada", "Ocupada"):
-            self.__status = status
+    def __init__(self, numero, capacidade):
+        self.numero = numero
+        self.capacidade = capacidade
+        self.ocupada = False
