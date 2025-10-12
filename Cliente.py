b03a1d04c0f208b0208755070b7051cf62c29623
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:18:54 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,12 @@
-from Usuario import Usuario
+class Cliente:
+    def __init__(self, login, nome, email):
+        self.login = login
+        self.nome = nome
+        self.email = email
+        self.reservas = []
 
-class Cliente(Usuario):
-    def __init__(self, nome: str, telefone: str, email: str):
-        super().__init__(nome, telefone, email)
+    def adicionar_reserva(self, reserva):
+        self.reservas.append(reserva)
+
+    def listar_reservas(self):
+        return self.reservas
