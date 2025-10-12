e4e9c5a3b689e7692b3cdba3f1c03c61231c73be
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-10-12 17:19:34 -0300

    Update Pagamento.py

--- Pagamento.py
+++ Pagamento.py
@@ -1,51 +1,9 @@
 class Pagamento:
-    def __init__(self, forma_pagamento: str, valor: float, data_pagamento: str, status: str):
-        self.__forma_pagamento = None
-        self.__valor = None
-        self.__data_pagamento = None
-        self.__status = None
+    def __init__(self, reserva_id, valor, metodo):
+        self.reserva_id = reserva_id
+        self.valor = valor
+        self.metodo = metodo  # ex: "cartao", "dinheiro"
+        self.status = "pendente"
 
-        if isinstance(forma_pagamento, str):
-            self.__forma_pagamento = forma_pagamento
-        if isinstance(valor, (int, float)):
-            self.__valor = float(valor)
-        if isinstance(data_pagamento, str):
-            self.__data_pagamento = data_pagamento
-        if isinstance(status, str):
-            self.__status = status
-
-    @property
-    def forma_pagamento(self):
-        return self.__forma_pagamento
-
-    @forma_pagamento.setter
-    def forma_pagamento(self, forma_pagamento: str):
-        if isinstance(forma_pagamento, str) and len(forma_pagamento) > 0:
-            self.__forma_pagamento = forma_pagamento
-
-    @property
-    def valor(self):
-        return self.__valor
-
-    @valor.setter
-    def valor(self, valor: float):
-        if isinstance(valor, (int, float)) and valor >= 0:
-            self.__valor = float(valor)
-
-    @property
-    def data_pagamento(self):
-        return self.__data_pagamento
-
-    @data_pagamento.setter
-    def data_pagamento(self, data_pagamento: str):
-        if isinstance(data_pagamento, str) and len(data_pagamento) > 0:
-            self.__data_pagamento = data_pagamento
-
-    @property
-    def status(self):
-        return self.__status
-
-    @status.setter
-    def status(self, status: str):
-        if isinstance(status, str) and len(status) > 0:
-            self.__status = status
+    def confirmar_pagamento(self):
+        self.status = "confirmado"
