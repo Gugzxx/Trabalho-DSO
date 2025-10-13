1fcb6abfca3a3e6e44c57f2034457d1934aece6e
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 11:48:08 -0300

    Update Cardapio.py

--- Objetos/Cardapio.py
+++ Objetos/Cardapio.py
@@ -1,10 +1,27 @@
 class Cardapio:
-    def __init__(self, restaurante_nome):
-        self.restaurante_nome = restaurante_nome
+    def __init__(self, restaurante_nome: str):
+        self.__restaurante_nome = None
         self.itens = []  
+
+        if isinstance(restaurante_nome, str):
+            self.__restaurante_nome = restaurante_nome
+    
+    @property
+    def restaurante_nome(self):
+        return self.__restaurante_nome
+    
+    @restaurante_nome.setter
+    def restaurante_nome(self, restaurante_nome):
+        self.__restaurante_nome = restaurante_nome
     
     def adicionar_item(self, item):
         self.itens.append(item)
     
     def listar_itens(self):
         return self.itens
+    
+    def remover_item(self, item):
+        if item in self.itens:
+            self.itens.remove(item)
+        else:
+            raise ValueError("Item não encontrado no cardápio.")
