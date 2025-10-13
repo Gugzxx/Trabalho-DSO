1909db63b7c1a6450080dee8f5446c5487582c30
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 11:46:18 -0300

    Update Restaurante.py

--- Objetos/Restaurante.py
+++ Objetos/Restaurante.py
@@ -1,9 +1,30 @@
 class Restaurante:
-    def __init__(self, nome, endereco):
-        self.nome = nome
-        self.endereco = endereco
+    def __init__(self, nome: str, endereco: str):
+        self.__nome = None
+        self.__endereco = None
         self.cardapio = None
         self.mesas = []
+
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(endereco, str):
+            self.__endereco = endereco
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @property
+    def endereco(self):
+        return self.__endereco
+    
+    @endereco.setter
+    def endereco(self, endereco: str):
+        self.__endereco = endereco
 
     def set_cardapio(self, cardapio):
         self.cardapio = cardapio
