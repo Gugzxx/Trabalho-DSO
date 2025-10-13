8436fa752c73fcf210a61b16b7f50e59377da123
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 14:17:45 -0300

    Update Funcionario.py

--- Objetos/Funcionario.py
+++ Objetos/Funcionario.py
@@ -1,24 +1,16 @@
-class Funcionario:
-    def __init__(self, login: str, nome: str, email: str, is_admin=False):
-        self.__login = None
+from Objetos.Usuario import Usuario
+
+class Funcionario(Usuario):
+    def __init__(self, login: str, senha: str, nome: str, email: str, is_admin=False):
+        super().__init__(login, senha, "funcionario")
         self.__nome = None
         self.__email = None
         self.is_admin = is_admin
 
-        if isinstance(login, str):
-            self.__login = login
         if isinstance(nome, str):
             self.__nome = nome
         if isinstance(email, str) and "@" in email:
             self.__email = email
-
-    @property
-    def login(self):
-        return self.__login
-    
-    @login.setter
-    def login(self, login: str):
-        self.__login = login
 
     @property
     def nome(self):
@@ -27,11 +19,11 @@ class Funcionario:
     @nome.setter
     def nome(self, nome: str):
         self.__nome = nome
-    
+
     @property
     def email(self):
         return self.__email
-    
+
     @email.setter
     def email(self, email: str):
         if "@" in email:
