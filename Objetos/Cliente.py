0efc869c3fad5631073d7e5e80551d9b93b2d780
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 11:47:37 -0300

    Update Cliente.py

--- Objetos/Cliente.py
+++ Objetos/Cliente.py
@@ -1,9 +1,43 @@
 class Cliente:
-    def __init__(self, login, nome, email):
-        self.login = login
-        self.nome = nome
-        self.email = email
+    def __init__(self, login: str, nome: str, email: str):
+        self.__login = None
+        self.__nome = None
+        self.__email = None
         self.reservas = []
+
+        if isinstance(login, str):
+            self.__login = login
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(email, str) and "@" in email:
+            self.__email = email
+
+    @property
+    def login(self):
+        return self.__login
+    
+    @login.setter
+    def login(self, login: str):
+        self.__login = login
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+    
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, email: str):
+        if "@" in email:
+            self.__email = email
+        else:
+            raise ValueError("Email inválido.")
 
     def adicionar_reserva(self, reserva):
         self.reservas.append(reserva)
