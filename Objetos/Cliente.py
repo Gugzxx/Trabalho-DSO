9d71d5eaf4c6f5bac620147a3b9a65466486020e
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 14:17:33 -0300

    Update Cliente.py

--- Objetos/Cliente.py
+++ Objetos/Cliente.py
@@ -1,37 +1,29 @@
-class Cliente:
-    def __init__(self, login: str, nome: str, email: str):
-        self.__login = None
+from Objetos.Usuario import Usuario
+
+class Cliente(Usuario):
+    def __init__(self, login: str, senha: str, nome: str, email: str):
+        super().__init__(login, senha, "cliente")
         self.__nome = None
         self.__email = None
         self.reservas = []
 
-        if isinstance(login, str):
-            self.__login = login
         if isinstance(nome, str):
             self.__nome = nome
         if isinstance(email, str) and "@" in email:
             self.__email = email
 
     @property
-    def login(self):
-        return self.__login
-    
-    @login.setter
-    def login(self, login: str):
-        self.__login = login
-    
-    @property
     def nome(self):
         return self.__nome
-    
+
     @nome.setter
     def nome(self, nome: str):
         self.__nome = nome
-    
+
     @property
     def email(self):
         return self.__email
-    
+
     @email.setter
     def email(self, email: str):
         if "@" in email:
