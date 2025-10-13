84ad68a52f5fcbfa901edded264fe466f0272831
Gugzxx <gustavo07genovez@gmail.com>
2025-10-13 11:47:08 -0300

    Update Mesa.py

--- Objetos/Mesa.py
+++ Objetos/Mesa.py
@@ -1,5 +1,32 @@
 class Mesa:
-    def __init__(self, numero, capacidade):
-        self.numero = numero
-        self.capacidade = capacidade
+    def __init__(self, numero: int, capacidade: int):
+        self.__numero = None
+        self.__capacidade = None
         self.ocupada = False
+
+        if isinstance(numero, int) and numero > 0:
+            self.__numero = numero
+        if isinstance(capacidade, int) and capacidade > 0:
+            self.__capacidade = capacidade
+
+    @property
+    def numero(self):
+        return self.__numero
+    
+    @numero.setter
+    def numero(self, numero: int):
+        if isinstance(numero, int) and numero > 0:
+            self.__numero = numero
+        else:
+            raise ValueError("Número da mesa deve ser um inteiro positivo.")
+        
+    @property
+    def capacidade(self):
+        return self.__capacidade
+    
+    @capacidade.setter
+    def capacidade(self, capacidade: int):
+        if isinstance(capacidade, int) and capacidade > 0:
+            self.__capacidade = capacidade
+        else:
+            raise ValueError("Capacidade da mesa deve ser um inteiro positivo.")
