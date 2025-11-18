9e966ad439af4b432193537b1e11e77058853ec3
Gugzxx <gustavo07genovez@gmail.com>
2025-11-18 14:28:03 -0300

    Update ControladorReserva to manage reservations
    Refactored ControladorReserva to manage reservations internally.

--- Controladores/controlador_reserva.py
+++ Controladores/controlador_reserva.py
@@ -1,9 +1,29 @@
-class ControladorReserva:
-    def __init__(self, sistema, tela_reserva):
-        self.sistema = sistema
-        self.tela_reserva = tela_reserva
+from Objetos.reserva import Reserva
 
-    def mostrar_todas_reservas(self):
-        reservas = self.sistema.listar_todas_reservas()
-        self.tela_reserva.mostrar_reservas(reservas)
-        input("Pressione Enter para continuar...")
+class ControladorReserva:
+    def __init__(self, tela_reserva):
+        self.tela_reserva = tela_reserva
+        self.reservas = [] 
+
+    def adicionar_reserva(self, reserva):
+        self.reservas.append(reserva)
+
+    def buscar_reserva(self, id_reserva):
+        for r in self.reservas:
+            if r.id == id_reserva:
+                return r
+        return None
+
+    def listar_todas_reservas(self):
+        self.tela_reserva.mostrar_reservas(self.reservas)
+
+    def listar_reservas_por_cliente(self, login_cliente):
+        lista_filtrada = [r for r in self.reservas if r.cliente_login == login_cliente]
+        return lista_filtrada
+
+    def remover_reserva(self, id_reserva, cliente_login):
+        reserva = self.buscar_reserva(id_reserva)
+        if reserva and reserva.cliente_login == cliente_login:
+            self.reservas.remove(reserva)
+            return True
+        return False
