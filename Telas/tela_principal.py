ea6311c0ffbfa2b9d0a76775c564355ff83779f7
Gugzxx <gustavo07genovez@gmail.com>
2025-11-18 14:21:54 -0300

    Refactor TelaPrincipal methods and remove menus

--- Telas/tela_principal.py
+++ Telas/tela_principal.py
@@ -1,29 +1,21 @@
 class TelaPrincipal:
+    def mostrar_menu_inicial(self):
+        print("\n=== SISTEMA DE RESERVAS ===")
+        print("1 - Login")
+        print("2 - Cadastrar Cliente")
+        print("0 - Sair")
+        return input("Escolha: ")
+
     def mostrar_login(self):
-        print("\n=== Tela de Login ===")
+        print("\n--- Login ---")
         login = input("Login: ")
         senha = input("Senha: ")
-        tipo = input("Tipo (cliente/funcionario): ").lower()
+        print("Entrar como:")
+        print("1 - Cliente")
+        print("2 - Funcionário")
+        tipo_opt = input("Opção: ")
+        tipo = "cliente" if tipo_opt == '1' else "funcionario"
         return login, senha, tipo
 
-    def mostrar_menu_cliente(self):
-        print("\nMenu Cliente:")
-        print("1. Listar reservas")
-        print("2. Criar reserva")
-        print("3. Alterar reserva")
-        print("4. Excluir reserva")
-        print("0. Logout")
-        escolha = input("Escolha: ")
-        return escolha
-
-    def mostrar_menu_funcionario(self):
-        print("\nMenu Funcionário:")
-        print("1. Gerenciar clientes")
-        print("2. Gerenciar funcionários")
-        print("3. Ver todas as reservas")
-        print("0. Logout")
-        escolha = input("Escolha: ")
-        return escolha
-
-    def mostrar_mensagem(self, texto):
-        print(texto)
+    def mostrar_mensagem(self, msg):
+        print(f"\n>>> {msg} <<<\n")
