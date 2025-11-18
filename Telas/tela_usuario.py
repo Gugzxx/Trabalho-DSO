8140d5fb998c11d8f6042a06b4f07df5f285187a
Gugzxx <gustavo07genovez@gmail.com>
2025-11-18 14:20:43 -0300

    Update tela_usuario.py

--- Telas/tela_usuario.py
+++ Telas/tela_usuario.py
@@ -1,12 +1,14 @@
-from Telas.AbstractTela import AbstractTela
+from Telas.abstract_tela import AbstractTela
 
 class TelaUsuario(AbstractTela):
     def pedir_dados_usuario(self):
+        print("--- Dados do Usuário ---")
         login = input("Login: ")
         senha = input("Senha: ")
-        nome = input("Nome: ")
+        nome = input("Nome Completo: ")
         email = input("Email: ")
         return login, senha, nome, email
-
-    def mostrar_mensagem(self, msg):
-        print(msg)
+    
+    def pedir_is_admin(self):
+        resp = input("É administrador? (s/n): ").lower()
+        return resp == 's'
