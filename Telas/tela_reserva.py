a741b700f4742d3f0d8ca755e8a80989031d89e5
Eduardo Virgílio Silva <112127058+virgiliooud@users.noreply.github.com>
2025-11-23 17:19:05 -0300

    Update tela_reserva.py

--- Telas/tela_reserva.py
+++ Telas/tela_reserva.py
@@ -1,14 +1,30 @@
+import FreeSimpleGUI as sg
 from Telas.abstract_tela import AbstractTela
 
 class TelaReserva(AbstractTela):
+    def __init__(self):
+        self.__window = None
+
     def mostrar_reservas(self, reservas):
-        print("\n--- Listagem de Reservas ---")
         if not reservas:
-            print("Nenhuma reserva encontrada.")
+            sg.popup("Nenhuma reserva encontrada.")
         else:
+            string_reservas = ""
             for r in reservas:
-                print(f"ID: {r.id} | Cliente: {r.cliente_login}")
-                print(f"   Local: {r.restaurante} - Mesa {r.mesa_numero}")
-                print(f"   Data: {r.data_hora}")
-                print(f"   Status: {r.status}")
-                print("-" * 30)
+                string_reservas += f"ID: {r.id} | Cliente: {r.cliente_login}\n"
+                string_reservas += f"   Local: {r.restaurante} - Mesa {r.mesa_numero}\n"
+                string_reservas += f"   Data: {r.data_hora}\n"
+                string_reservas += f"   Status: {r.status}\n"
+                string_reservas += "-" * 30 + "\n"
+            
+            sg.Popup('--- LISTAGEM DE RESERVAS ---', string_reservas)
+
+    def close(self):
+        if self.__window:
+            self.__window.Close()
+
+    def open(self):
+        if self.__window:
+            button, values = self.__window.Read()
+            return button, values
+        return None, None
